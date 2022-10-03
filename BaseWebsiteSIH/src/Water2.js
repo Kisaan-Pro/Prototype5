3d4db912ec6469b127a56b8c8b9a1f6f8de27e86
vidushi07 <76913381+vidushi07@users.noreply.github.com>
2022-10-04 00:11:37 +0530

    Made the elements responsive and improved the design

--- BaseWebsiteSIH/src/Water2.js
+++ BaseWebsiteSIH/src/Water2.js
@@ -1,29 +1,33 @@
 import React from "react";
-import {useState} from "react";
-import Water from './Water'
+import { useState } from "react";
+import Water from "./Water";
 
 const Water2 = (props) => {
-    return(
-        (props.toDisplay=="Water2") && 
-        <div className = {props.wname}>
-        {props.wname == "red" ?
-        <div>
-            <div className="midsquare" style={{ padding: "10%"}}>
-          <Water name='redTemp'/>
+  return (
+    props.toDisplay == "Water2" && (
+      <div className={props.wname}>
+        {props.wname == "red" ? (
+          <div>
+            <div className="midsquare" style={{ padding: "10%" }}>
+              <Water name="redTemp" />
             </div>
 
-            <div style={{margin: 'auto'}}>
-            <h2>Solution: </h2>
-         <h3>
-            <ul> Start irrigation pump immediately</ul>
-          </h3>
+            <div style={{ margin: "auto" }}>
+              <h2>Solution: </h2>
+              <br></br>
+              <h5>
+                <ul> Start irrigation pump immediately</ul>
+              </h5>
             </div>
-        </div> :
-        <div className="midsquare" style={{ padding: "10%"}}>
-          <Water name='greenTemp'/>
-            </div>}
+          </div>
+        ) : (
+          <div className="midsquare" style={{ padding: "10%" }}>
+            <Water name="greenTemp" />
+          </div>
+        )}
       </div>
-    );
-}
+    )
+  );
+};
 
 export default Water2;
--- BaseWebsiteSIH/src/components/CitySelector.js
+++ BaseWebsiteSIH/src/components/CitySelector.js
@@ -1,33 +1,45 @@
-import React, {useState} from 'react';
-import {Row, Col, FormControl, Button} from 'react-bootstrap';
-const CitySelector = ({onSearch}) => {
-    const [city, setCity] = useState('');
+import React, { useState } from "react";
+import { Row, Col, FormControl, Button } from "react-bootstrap";
+const CitySelector = ({ onSearch }) => {
+  const [city, setCity] = useState("");
 
-    return (
-      <>
-        <Row>
-          <Col>
-            <h1>Search your city</h1>
-          </Col>
-        </Row>
+  return (
+    <>
+      <Row>
+        <Col>
+          <br></br>
+          <h2>Search your city</h2>
+          <br></br>
+        </Col>
+      </Row>
 
-        <Row>
-          <Col xl={5}>
-            <FormControl
-              placeholder="Enter city"
-              onChange={(event) => setCity(event.target.value)}
-              value={city}
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <Button onClick={() => onSearch(city)}>Check Weather</Button>
-          </Col>
-        </Row>
-      </>
-    );
-  };
+      <Row>
+        <Col xl={5} style={{ marginLeft: "auto", marginRight: "auto" }}>
+          <FormControl
+            style={{ borderWidth: "3px", fontSize: "medium" }}
+            placeholder="Enter city"
+            onChange={(event) => setCity(event.target.value)}
+            value={city}
+          />
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col style={{ paddingBottom: "20px" }}>
+          <Button
+            onClick={() => onSearch(city)}
+            style={{
+              backgroundColor: "#0c023e",
+              width: "200px",
+              fontSize: "medium",
+            }}
+          >
+            Check Weather
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default CitySelector;
